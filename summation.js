5d8b8031b75bac1faad62b8a5a629a0c68d90e43
Mehmet Duran Ataseven <mataseven@bimser.com.tr>
2023-08-25 09:48:18 +0300

    Proje oluşturuldu

---
+++ summation.js
@@ -0,0 +1,9 @@
+function sum() {
+  var number1 = document.getElementById("number1").value;
+  var number2 = document.getElementById("number2").value;
+
+  var sum = parseInt(number1) + parseInt(number2);
+
+  var result = document.getElementById("result");
+  result.innerText = result;
+}
