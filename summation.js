8e79cfdf499d99aa656650766ace65c689f7d522
Mehmet Duran Ataseven <mataseven@bimser.com.tr>
2023-08-25 09:59:10 +0300

    Sayının 100'den büyük olma kontrolü yapıldı

--- summation.js
+++ summation.js
@@ -3,7 +3,10 @@ function sum() {
   var number2 = document.getElementById("number2").value;
 
   var sum = parseInt(number1) + parseInt(number2);
-
+  if (sum > 100) {
+    alert("Toplam 100'den büyük olamaz");
+    return;
+  }
   var result = document.getElementById("result");
   result.value = sum;
 }
